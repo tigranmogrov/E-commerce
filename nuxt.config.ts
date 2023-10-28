571234fee1e94fafdb7a14e2d400a8fe546d5efe
Tigran Mogrov <tigranmogrov@gmail.com>
2023-10-28 15:52:03 +0300

    feat: add eslint and prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/eslint-module'],
+  eslint: {
+    lintOnStart: false,
+    exclude: ['**/node_modules/**'],
+  },
   devtools: { enabled: false },
   srcDir: './client',
   serverDir: './server',
   imports: {
     dirs: ['stores', '~stores', '~node_modules', '~server', '~public'],
   },
-})
+});
